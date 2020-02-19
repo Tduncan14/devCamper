1f25d713d56fb71bde8c4f555459d43a761b5bbf
Treek <nbatre1@yahoo.com>
2020-02-19 16:39:02 -0600

    keep moving forward

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,12 +2,60 @@
 
 // to get all the bootcamps
 //@route GET/api/vi/bootcamps
-//@access
+//@access public
 
 exports.getBootcamps =(req,res,next) =>{
 
   res.status(200).json({success:true, msg:"Show all bootcamps"})
 
 
+
+}
+
+// get a single bootcamp
+// @route  Get/ api/v1/bootcamps/:id
+//access Public
+
+exports.getBootcamps = (req,res,ext) => {
+    res.status(200).json({success:true,msg:`show the ${req.params.id}`})
+}
+
+
+// Put create a bootcamp
+//@route Post/api/v1/bootcamps
+//private meaans you have to logged in
+
+
+exports.createBootcamp = (req,res,next) =>{
+
+    res.status(200).json({
+        success:true, msg:`Create new bootcamp ${req.params.id}`
+    })
+
+}
+
+// update bootcamp
+//route Put /api/v1/bootcamps/:id
+//@access Private
+
+exports.updateBootcamp = (req,res,next) =>{
+
+    res.status(200).json({
+        success:true, msg: `this is the ${req.params.id}`
+    })
+
+ 
+}
+
+
+// delete bootcamp
+//@ route Delete /api/v1/bootcamps/:id
+//@access Private
+exports.deleteBootcamp = (req,res,next) =>{
+
+    res.status(200).json({
+        success:true,
+        msg:`this is to delete the bootcamp ${req.params.id}`
+    })
 
 }
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -7,34 +7,27 @@ const router = express.Router()
 
 
 router.get('/',(req,res) =>{
-    res.status(200).json({success:true , msg:'show all'})
+
 })
 
 router.get('/:id',(req,res) =>{
-    res.status(200).json({success:true , id: req.params.id})
+  
 })
 
 
-router.post('/:id',(req,res) =>{
-   res.status(200).json({
-       success:true, msg:`Create new bootcamp ${req.params.id}`
-   })
+router.post('/',(req,res) =>{
+  
 })
 
 
 router.put('/:id',(req,res) =>{
-   res.status(200).json({
-       success:true, msg: `this is the ${req.params.id}`
-   })
+  
 })
 
 
 router.delete('/:id',(req,res) =>{
    
-   res.status(200).json({
-       success:true,
-       msg:`this is to delete the bootcamp ${req.params.id}`
-   })
+  
 })
 
 
